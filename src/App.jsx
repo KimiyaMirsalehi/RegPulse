04945ad9870f6990fcff12e4bc7afa2cf69145fe
Kimiya Mirsalehi <kimiya.mirsalehi@gmail.com>
2026-06-08 21:03:50 +0300

    Fix React production runtime

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import publicationsData from '../data/publications.json';
 
 const PAGE_SIZE = 10;
